9089de35e645858a9cc5a74950714fce3c9e7451
manojdeepak200 <115771499+manojdeepak200@users.noreply.github.com>
2024-07-30 23:45:54 +0530

    Update Tomcatfile.sh

--- Tomcatfile.sh
+++ Tomcatfile.sh
@@ -1,9 +1,9 @@
 cd /opt
 sudo wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.91/bin/apache-tomcat-9.0.91.tar.gz
 sudo tar -xvf /opt/apache-tomcat-9.0.91.tar.gz
-cd /opt/apache-tomcat-9.0.84/webapps/manager/META-INF
+cd /opt/apache-tomcat-9.0.91/webapps/manager/META-INF
 sudo sed -i 's/"127\\.\\d+\\.\\d+\\.\\d+|::1|0:0:0:0:0:0:0:1"/".*"/g' context.xml
-cd /opt/apache-tomcat-9.0.84/conf
+cd /opt/apache-tomcat-9.0.91/conf
 sudo mv tomcat-users.xml tomcat-users_bkup.xml
 sudo touch tomcat-users.xml
 sudo echo '<?xml version="1.0" encoding="utf-8"?>
@@ -12,6 +12,6 @@ sudo echo '<?xml version="1.0" encoding="utf-8"?>
         <user username="tomcat" password="Tomcat" roles="manager-gui, manager-script, manager-status"/>
         </tomcat-users>' > tomcat-users.xml
 
-cd /opt/apache-tomcat-9.0.84/conf/
+cd /opt/apache-tomcat-9.0.91/conf/
 sudo sed -i 's/Connector port="8080"/Connector port="8081"/g' server.xml
-sudo /opt/apache-tomcat-9.0.84/bin/startup.sh
+sudo /opt/apache-tomcat-9.0.91/bin/startup.sh
